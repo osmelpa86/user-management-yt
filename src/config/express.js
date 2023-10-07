34fc37daf92d4832fe8f9fb0c3a9a0cb11afaf19
Osmel <osmelpa86@gmail.com>
2023-10-07 14:58:01 -0400

    Schemas and routes

--- src/config/express.js
+++ src/config/express.js
@@ -1,6 +1,12 @@
+import userRouter from '#Routes/user.routes.js';
 import express from 'express';
 
 const expressApp = express();
 
-// TODO: Añadir middleware, routes...
+// Middleware
+expressApp.use(express.json());
+
+// Routes
+expressApp.use('/user', userRouter);
+
 export default expressApp;
